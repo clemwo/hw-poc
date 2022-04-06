82b0b5503e76f0e3ffec9e4c822ab95f249239e1
Clemens Wodtke <clemwo@mailbox.org>
2022-04-06 18:14:35 +0200

    add alert message after successful entry of project

--- src/projects/views.py
+++ src/projects/views.py
@@ -2,6 +2,7 @@ from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 from .models import Project
 from .create_project_form import CreateProjectForm
+from django.contrib import messages
 
 # List all the posts
 def project_list_view(request):
@@ -20,6 +21,7 @@ def create_project_view(request):
         if create_project_form.is_valid():
             project = create_project_form.save(commit=False)
             project.save()
+            messages.success(request, "Projekt erfolgreich eingereicht")
             return HttpResponseRedirect('')
     else:
         create_project_form = CreateProjectForm()
